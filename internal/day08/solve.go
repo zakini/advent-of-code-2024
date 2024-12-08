548fab49715aa2f4e844bb78b2d998f4e9bcfb8e
Josh Wells <josh@parall.ax>
2024-12-08 21:33:06 +0000

    Solved day 8 part 2

--- internal/day08/solve.go
+++ internal/day08/solve.go
@@ -31,6 +31,10 @@ func (a Vector2) subtract(b Vector2) Vector2 {
 	}
 }
 
+func (a Vector2) clone() Vector2 {
+	return Vector2{a.x, a.y}
+}
+
 func SolvePart1(input string, debug bool) int {
 	worldBounds, antennae := parseInput(input)
 
@@ -49,22 +53,70 @@ func SolvePart1(input string, debug bool) int {
 		for i, a := range targetAntennae {
 			for _, b := range targetAntennae[i+1:] {
 				distance := b.position.subtract(a.position)
-				antiNodes = append(
-					antiNodes,
-					a.position.subtract(distance),
-					b.position.add(distance),
-				)
+
+				antiNodeA := a.position.subtract(distance)
+				antiNodeB := b.position.add(distance)
+
+				if pointInBounds(antiNodeA, worldBounds) {
+					antiNodes = append(antiNodes, antiNodeA)
+				}
+				if pointInBounds(antiNodeB, worldBounds) {
+					antiNodes = append(antiNodes, antiNodeB)
+				}
 			}
 		}
 
 		resolvedFrequencies = append(resolvedFrequencies, antenna.frequency)
 	}
 
-	// Remove anti-nodes that are out of bounds
-	antiNodes = utils.Filter(antiNodes, func(_ int, antiNode Vector2) bool {
-		return 0 <= antiNode.x && antiNode.x < worldBounds.x &&
-			0 <= antiNode.y && antiNode.y < worldBounds.y
+	// Remove duplicate anti-nodes
+	antiNodes = utils.Filter(antiNodes, func(i int, antiNode Vector2) bool {
+		return i == slices.Index(antiNodes, antiNode)
 	})
+
+	if debug {
+		printWorld(worldBounds, antennae, antiNodes)
+	}
+
+	return len(antiNodes)
+}
+
+func SolvePart2(input string, debug bool) int {
+	worldBounds, antennae := parseInput(input)
+
+	antiNodes := make([]Vector2, 0)
+	resolvedFrequencies := make([]string, 0)
+	for _, antenna := range antennae {
+		if slices.Contains(resolvedFrequencies, antenna.frequency) {
+			// Already checked antennae with this frequency, skip this antenna
+			continue
+		}
+
+		targetAntennae := utils.Filter(antennae, func(_ int, a Antenna) bool {
+			return a.frequency == antenna.frequency
+		})
+
+		for i, a := range targetAntennae {
+			for _, b := range targetAntennae[i+1:] {
+				distance := b.position.subtract(a.position)
+
+				antiNode := a.position.clone()
+				for pointInBounds(antiNode, worldBounds) {
+					antiNodes = append(antiNodes, antiNode)
+					antiNode = antiNode.add(distance)
+				}
+
+				antiNode = a.position.clone().subtract(distance)
+				for pointInBounds(antiNode, worldBounds) {
+					antiNodes = append(antiNodes, antiNode)
+					antiNode = antiNode.subtract(distance)
+				}
+			}
+		}
+
+		resolvedFrequencies = append(resolvedFrequencies, antenna.frequency)
+	}
+
 	// Remove duplicate anti-nodes
 	antiNodes = utils.Filter(antiNodes, func(i int, antiNode Vector2) bool {
 		return i == slices.Index(antiNodes, antiNode)
@@ -115,6 +167,11 @@ func vector2Compare(a Vector2, b Vector2) int {
 
 func antennaPositionCompare(a Antenna, b Antenna) int {
 	return vector2Compare(a.position, b.position)
+}
+
+func pointInBounds(point Vector2, bounds Vector2) bool {
+	return 0 <= point.x && point.x < bounds.x &&
+		0 <= point.y && point.y < bounds.y
 }
 
 func printWorld(worldBounds Vector2, antennae []Antenna, antiNodes []Vector2) {
